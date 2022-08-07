772c202ff20f7fac837f8e0a12d9f1a51c4c9b5b
Oleguzik <guzik.oleg@gmail.com>
2022-08-07 12:11:44 +0200

    with ready modalWin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,6 +5,7 @@ import { galleryItems } from './gallery-items.js'
 
 const galleryMainItem = document.querySelector('.gallery')
 const galleryMarkUp = galleryItems.map(createImageGridMarkup).join('')
+let simpleModal
 
 galleryMainItem.insertAdjacentHTML('afterbegin', galleryMarkUp)
 galleryMainItem.addEventListener('click', onGalleryClick)
@@ -23,7 +24,70 @@ function createImageGridMarkup({ preview, original, description } = {}) {
 			</div>`
 }
 
-function onGalleryClick(e) {
-  e.preventDefault()
-  console.log(e.currentTarget)
+function onGalleryClick(event) {
+  event.preventDefault()
+  // console.log(event.target.dataset.source)
+  openModalWin(event.target.dataset.source)
+}
+
+function openModalWin(src) {
+  if (simpleModal?.visible()) {
+    simpleModal.close()
+  }
+
+  simpleModal = basicLightbox.create(
+    `<img src="${src}" width="1280" alt="original">`,
+    {
+      onClose: () => {
+        window.removeEventListener('keydown', onKeyboardClick)
+      },
+    }
+  )
+  simpleModal.show()
+  window.addEventListener('keydown', onKeyboardClick)
+  // console.log(simpleModal.element().querySelector('img').src);
+}
+
+function closeModal() {
+  simpleModal.close()
+}
+
+function onKeyboardClick(event) {
+  const oldSrcIndex = findCurrentSrcIndex(
+    simpleModal.element().querySelector('img').src
+  )
+
+  // console.log('oldSrcIndex', oldSrcIndex)
+
+  switch (event?.code) {
+    case 'Escape':
+      closeModal()
+      return
+
+    case 'ArrowLeft':
+    case 'ArrowDown':
+      openModalWin(
+        galleryItems[
+          oldSrcIndex === 0 ? galleryItems.length - 1 : oldSrcIndex - 1
+        ].original
+      )
+      break
+
+    case 'ArrowRight':
+    case 'ArrowUp':
+    case 'Space':
+      openModalWin(
+        galleryItems[
+          oldSrcIndex === galleryItems.length - 1 ? 0 : oldSrcIndex + 1
+        ].original
+      )
+      break
+  }
+}
+
+function findCurrentSrcIndex(src) {
+  for (let i = 0; i < galleryItems.length; i += 1) {
+    if (galleryItems[i].original === src) return i
+  }
+  return
 }
