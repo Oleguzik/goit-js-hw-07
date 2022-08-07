bd02a85f5aff08662fbd4f8f1a0eeda8b0a663f4
Oleguzik <guzik.oleg@gmail.com>
2022-08-07 21:15:28 +0200

    Lightbox release

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
-console.log(galleryItems)
+// console.log(galleryItems)
+
+const galleryMeinItem = document.querySelector('.gallery')
+const galleryMarkUp = galleryItems.map(createImageGridMarkup).join('')
+
+galleryMeinItem.insertAdjacentHTML('afterbegin', galleryMarkUp)
+
+function createImageGridMarkup({ preview, original, description } = {}) {
+  return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/></a>`
+}
+
+;(function () {
+  let $gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 })
+})()
